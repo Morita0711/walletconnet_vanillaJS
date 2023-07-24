73f375134cf693bc113c16d3add20c7992815728
Morita0711 <moritaamaya0711@gmail.com>
2023-07-24 08:03:20 +0200

    add: buy eth | USDT

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 
+console.log(__dirname, './src/index.html')
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '1.html'));
+  res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
